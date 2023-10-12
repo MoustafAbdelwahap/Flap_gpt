9469edf3ca0b2d1cd1031a61870c73adc76455f1
MoustafAbdelwahap <78971252+MoustafAbdelwahap@users.noreply.github.com>
2023-10-12 19:56:56 +0300

    Update Flap_gpt.py

--- Flap_gpt.py
+++ Flap_gpt.py
@@ -1,25 +1,35 @@
 import openai
 import streamlit as st
+import requests
 
-openai.api_key = 'bDnI7LKi0Z27wG4QFJihT3BlbkFJBEBGgKoHiZ7In8f6AU7Z'
+def generate_description(input_text):
+    url = "https://chatgptapifree.herokuapp.com/generate"
+    payload = {
+        "model": "text-davinci-003",
+        "prompt": f"Write a product description based on the below information.\n\n{input_text}\n\nDescription:",
+        "temperature": 0.7,
+        "max_tokens": 256,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0
+    }
+
+    response = requests.post(url, json=payload)
+
+    if response.status_code == 200:
+        return response.json().get("choices", [])[0].get("text", "")
+    else:
+        return "Failed to generate description. Please try again."
 
 def main():
     st.title("Product Description Generator")
     notes = st.text_area("Enter product information:")
+
     if st.button("Generate description"):
         with st.spinner("Generating description..."):
-            response = openai.Completion.create(
-              model="text-davinci-003",
-              prompt=f"Write a product description based on the below information.\n\n{notes}\n\nDescription:",
-              temperature=0.7,
-              max_tokens=256,
-              top_p=1,
-              frequency_penalty=0,
-              presence_penalty=0
-            )
-        description = response['choices'][0]['text']
-        st.subheader("Generated description:")
-        st.write(description)
+            description = generate_description(notes)
+            st.subheader("Generated description:")
+            st.write(description)
 
 if __name__ == "__main__":
     main()
